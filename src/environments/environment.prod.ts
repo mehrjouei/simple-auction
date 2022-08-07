27e6ccfc0da288cceb1869177c3d4541e66beb5a
vahid mehrjouei <mehrjooei.v@gmail.com>
2022-08-07 23:18:41 +0200

    Add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDeR9Ua-Y2-STzzQrRcDyESyZ2TZYmPTsY',
+    authDomain: 'simple-auction-544b7.firebaseapp.com',
+    projectId: 'simple-auction-544b7',
+    storageBucket: 'simple-auction-544b7.appspot.com',
+    messagingSenderId: '519456841587',
+    appId: '1:519456841587:web:774723fe5eb7de96536f06',
+    measurementId: 'G-22FQ7C909E',
+  },
 };
